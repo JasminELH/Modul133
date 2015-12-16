521d4a7beb45b7b1c31f4e4184a08a8d2285e85c
JasminELH <jasmin.el@hotmail.ch>
2015-12-16 08:46:05 +0100

    Create loginform.php

---
+++ loginform.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Loginform</title>
+		<meta charset="UTF-8">	
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<header>
+			<h1>Login</h1>		
+		</header>
+		<div>
+			<form action="login.php" method="post">
+				<label for="user">Username</label>
+				<input type="text" name="user" id="user" required>
+				<br>
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" required>
+				<br>
+				<a href="registerform.php">Registrieren</a><br>
+				<input type="submit" value="login">
+			</form>
+		</div>
+	</body>
+</html>
